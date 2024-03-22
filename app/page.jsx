e0c179c7728a2103cde219473b9af73390727585
VarakinV <152953525+VarakinV@users.noreply.github.com>
2024-03-22 17:47:32 -0600

    Created layout, home page and styles

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// export const metadata = {
+//     title: 'Home Page',
+//     description: '',
+// }
+
+const HomePage = () => {
+    return (
+        <div>Home</div>
+    )
+}
+
+export default HomePage
