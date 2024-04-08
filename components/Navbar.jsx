aa70a71dc8313440803f0a5577a9879b4b5d17ff
VarakinV <152953525+VarakinV@users.noreply.github.com>
2024-04-07 20:33:43 -0600

    Add Property Form Submission

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,6 +11,7 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Navbar = () => {
     const { data: session } = useSession();
+    const profileImage = session?.user?.image;
 
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
@@ -26,6 +27,7 @@ const Navbar = () => {
 
         setAuthProviders();
     }, []);
+
 
     return (
         <nav className="bg-blue-700 border-b border-blue-500">
@@ -161,8 +163,10 @@ const Navbar = () => {
                                         <span className="sr-only">Open user menu</span>
                                         <Image
                                             className="h-8 w-8 rounded-full"
-                                            src={profileDefault}
+                                            src={profileImage || profileDefault}
                                             alt=""
+                                            width={40}
+                                            height={40}
                                         />
                                     </button>
                                 </div>
@@ -183,6 +187,9 @@ const Navbar = () => {
                                             role="menuitem"
                                             tabIndex="-1"
                                             id="user-menu-item-0"
+                                            onClick={() => {
+                                                setIsProfileMenuOpen(false);
+                                            }}
                                         >Your Profile</Link>
                                         <Link
                                             href="/properties/saved"
@@ -190,8 +197,15 @@ const Navbar = () => {
                                             role="menuitem"
                                             tabIndex="-1"
                                             id="user-menu-item-2"
+                                            onClick={() => {
+                                                setIsProfileMenuOpen(false);
+                                            }}
                                         >Saved Properties</Link>
                                         <button
+                                            onClick={() => {
+                                                setIsProfileMenuOpen(false);
+                                                signOut();
+                                            }}
                                             className="block px-4 py-2 text-sm text-gray-700"
                                             role="menuitem"
                                             tabIndex="-1"
